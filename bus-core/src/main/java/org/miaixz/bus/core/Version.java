9317abe8bd0aaac95329ea65152f88a810059fe6
Kimi Liu <839536@qq.com>
2024-11-10 09:36:17 +0800

    v8.1.2

--- bus-core/src/main/java/org/miaixz/bus/core/Version.java
+++ bus-core/src/main/java/org/miaixz/bus/core/Version.java
@@ -48,7 +48,7 @@ public class Version implements Comparable<Version>, Serializable {
     /**
      * 版本信息
      */
-    public static final String _VERSION = "8.1.1";
+    public static final String _VERSION = "8.1.2";
 
     private static final long serialVersionUID = -1L;
     private final String version;
