d5f84c7ac98fc0bf2df2e267c77070dbe3d534ac
Kimi Liu <839536@qq.com>
2025-06-23 12:09:33 +0800

    :boom: v8.2.1

--- bus-core/src/main/java/org/miaixz/bus/core/Version.java
+++ bus-core/src/main/java/org/miaixz/bus/core/Version.java
@@ -52,7 +52,7 @@ public class Version implements Comparable<Version>, Serializable {
     /**
      * 版本信息
      */
-    public static final String _VERSION = "8.2.0";
+    public static final String _VERSION = "8.2.1";
 
     private final String version;
     private final List<Object> sequence;
